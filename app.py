bf224cee853bfa798066a35603fb726b43e59858
xankap <106265631+xankap@users.noreply.github.com>
2026-01-30 14:39:55 +0100

    Added ripe tomatoes model to the streamlit app

--- app.py
+++ app.py
@@ -46,6 +46,9 @@ from PIL import Image
 # Load model
 model = tf.keras.models.load_model("/content/cats_dogs_model.h5")
 
+# Load model
+model2 = tf.keras.models.load_model("best.onnx")
+
 st.title("🐱🐶 Cats vs Dogs Classifier")
 st.write("Upload an image and the model will predict whether it is a cat or a dog.")
 
@@ -64,3 +67,22 @@ if uploaded_file is not None:
         st.success("🐶 This is a Dog!")
     else:
         st.success("🐱 This is a Cat!")
+
+st.title("Ripe and unripe tomato classifier")
+st.write("Upload an image and the model will predict whether tomato is ripe or not.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).resize((150, 150))
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img_array = np.array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model2.predict(img_array)[0][0]
+
+    if prediction > 0.5:
+        st.success("Tomato is ripe!")
+    else:
+        st.success("Tomatoe is unripe")
